6b6fb1829c7e0870c81bf1fbd04ed82dcdc8b194
locapeesh <92260919+locapeesh@users.noreply.github.com>
2021-11-17 16:39:04 +0100

    Final commit

--- MyWindow.java
+++ MyWindow.java
@@ -1,3 +1,6 @@
+/* Proszę podawać Andrii jako login i 262701 jako hasło,
+ żeby sprawdzić poprawność programu */
+
 package lab3;
 
 import javax.swing.*;
@@ -17,76 +20,77 @@ public class MyWindow extends JFrame implements ActionListener {
     JTextField userLogin;
     JPasswordField userPassword;
 
-    public void run(){ contentPanel = new JPanel();
-    ImageIcon image = new ImageIcon("smile.png");
-    this.setTitle("Sign up");
-    this.setSize(280,210);
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    this.setResizable(false);
-    this.setLocationRelativeTo(null);
-    this.setIconImage(image.getImage());
-    this.add(contentPanel);
+    public void run() {
+        contentPanel = new JPanel();
+        ImageIcon image = new ImageIcon("smile.png");
+        this.setTitle("Sign up");
+        this.setSize(280, 210);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setIconImage(image.getImage());
+        this.add(contentPanel);
 
 
-    contentPanel.setLayout(null);
-    JLabel label1 = new JLabel("User");
-    label1.setBounds(35,20,60,20);
-    contentPanel.add(label1);
-    userLogin = new JTextField(10);
-    userLogin.setBounds(120,20,80,20);
-    contentPanel.add(userLogin);
+        contentPanel.setLayout(null);
+        JLabel label1 = new JLabel("User");
+        label1.setBounds(35, 20, 60, 20);
+        contentPanel.add(label1);
+        userLogin = new JTextField(10);
+        userLogin.setBounds(120, 20, 80, 20);
+        contentPanel.add(userLogin);
 
 
-    JLabel label2 = new JLabel("Password");
-    label2.setBounds(35,60,60,20);
-    contentPanel.add(label2);
-    userPassword = new JPasswordField(10);
-    userPassword.setBounds(120,60,80,20);
-    contentPanel.add(userPassword);
+        JLabel label2 = new JLabel("Password");
+        label2.setBounds(35, 60, 60, 20);
+        contentPanel.add(label2);
+        userPassword = new JPasswordField(10);
+        userPassword.setBounds(120, 60, 80, 20);
+        contentPanel.add(userPassword);
 
 
-    signUp = new JButton("Sign up ");
-    signUp.setBounds(10,110,80,20);
-    signUp.addActionListener(this);
-    clear = new JButton("Clear all ");
-    clear.setBounds(120,110,100,22);
-    clear.addActionListener(this);
-    contentPanel.add(signUp);
-    contentPanel.add(clear);
+        signUp = new JButton("Sign up ");
+        signUp.setBounds(10, 110, 80, 20);
+        signUp.addActionListener(this);
+        clear = new JButton("Clear all ");
+        clear.setBounds(120, 110, 100, 22);
+        clear.addActionListener(this);
+        contentPanel.add(signUp);
+        contentPanel.add(clear);
 
-    success.setBounds(20, 140,200,20);
-    contentPanel.add(success);
-    failure.setBounds(20,140,200,20);
-    contentPanel.add(failure);
+        success.setBounds(20, 140, 200, 20);
+        contentPanel.add(success);
+        failure.setBounds(20, 140, 200, 20);
+        contentPanel.add(failure);
 
-        user.add(new User("Zhukov","262701"));
-        user.add(new User("Pochelov","390210"));
-        user.add(new User("Gwiazda","211234"));
-        user.add(new User("Duda","325678"));
-        user.add(new User("Kloc","413425"));
+        user.add(new User("Zhukov", "262701"));
+        user.add(new User("Pochelov", "390210"));
+        user.add(new User("Gwiazda", "211234"));
+        user.add(new User("Duda", "325678"));
+        user.add(new User("Kloc", "413425"));
 
         this.setVisible(true);
-}
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String clickedButton = e.getActionCommand();
         System.out.println("Button " + clickedButton + "clicked");
-        if(e.getSource() == signUp){
-            for(User user : user){
-                if(Objects.equals(userLogin.getText(),user.getLogin())){
-                    if(Objects.equals(String.valueOf(userPassword.getPassword()), user.getPassword())){
+        if (e.getSource() == signUp) {
+            for (User user : user) {
+                if (Objects.equals(userLogin.getText(), user.getLogin())) {
+                    if (Objects.equals(String.valueOf(userPassword.getPassword()), user.getPassword())) {
                         signUp.setBackground(Color.GREEN);
                         success.setText("You have successfully signed up!");
-                    } else {signUp.setBackground(Color.red);
-                    failure.setText("Something went wrong..Try again");}
+                    } else {
+                        signUp.setBackground(Color.red);
+                        failure.setText("Something went wrong..Try again");
+                    }
                 }
             }
 
 
-
-
-        } else if (e.getSource() == clear){
+        } else if (e.getSource() == clear) {
             userLogin.setText(null);
             userPassword.setText(null);
             signUp.setBackground(null);
